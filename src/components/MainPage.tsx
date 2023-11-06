aae6faa222e5d29b3ab484f69a76d793b5592507
anjali-amazatic <anjali.u@amazatic.com>
2023-11-06 16:08:32 +0530

    Change images layout...

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,86 +1,87 @@
 import React from 'react'
-import { IMG1, IMG2, IMG3, IMG4, IMG5, 
-    IMG6, IMG7, IMG8, IMG10, IMG11,
-    IMG13, IMG14, IMG15, IMG16, IMG17,
-    IMG19, IMG20, IMG22, IMG23, IMG24,
-    IMG25, IMG26, IMG27, IMG28, IMG29,
-    IMG30, IMG31, IMG32, IMG33, IMG34,
-    IMG35, IMG36, IMG37, IMG38, MP4Animations } from '../images'
+import {  MP4Animations } from '../images'
 import Lottie from 'lottie-react'
-
+import styled from 'styled-components';
+import images from '../elements/images';
 const MainPage = () => {
-    const images = [IMG26, IMG25, IMG27,
-                    IMG22, IMG17, IMG16,
-                    IMG29, IMG1, IMG30,
-                    IMG14, IMG19,IMG15,
-                    IMG31, IMG3, IMG32,
-                    IMG11, IMG8, IMG13,
-                    IMG33, IMG4, IMG34,
-                    IMG2, IMG7, IMG10,
-                    IMG35, IMG5,IMG36,
-                    IMG23, IMG20, IMG24,
-                    IMG37, IMG6,IMG38,
-                    IMG28]
-
     const renderImage = () =>{
         return images.map((imageFile, index)=>(
-            <div key={index} className="col-md-6 col-sm-6 col-lg-4 mb-2">
+            <div key={index} className="col-md-4 col-sm-4 col-lg-4 col-xs-6 mb-2">
                 <div className="card animate__animated animate__backInLeft animate__slideInUp" style={{ 
                     width: '100%',
                     height:'100%',
                     boxShadow:'0px 29px 52px 0px rgba(0,0,0,0.4)',
-                    borderRadius:'35px',
+                    backgroundColor:'white'
                     }}>
-                    <img src={imageFile} className="card-img-top animate__animated animate__zoomIn" alt={`Image_${index}`}
-                    style={{borderRadius:'35px', height:'100%'}} />
+                    <img src={imageFile.image} className="card-img-top animate__animated animate__zoomIn" alt={`Image_${index}`}
+                    style={{ height:'100%'}} />
+                    <div className='card-title text-center mt-2'>
+                        Type:&nbsp;<strong>{imageFile.title}</strong>
                     </div>
                 </div>
+            </div>
         ))
     }
   return (
-    <div className='container'>
-        <div className='row'>
-            <div className="col-lg-4 col-sm-4 col-md-4">
+    <MainComponet>
+        <div className='container'>
+            <div className='row '>
+                <div className="col-lg-4 col-sm-4 col-md-4 col-xs-4 ">
+                    <Lottie
+                    animationData={MP4Animations}
+                    className='animate__animated animate__bounce'
+                    style={{
+                    width: "200px",
+                    margin: "0 auto",
+                    marginBottom:'-45px'
+                    }}
+                    />
+                </div>
+                <div className="col-lg-4 col-sm-4 col-md-4 col-xs-4 ">
+                    <div className="icon-container">
+                        <section
+                            className='animate__animated animate__backInRight'
+                            style=
+                            {{textAlign:'center',
+                            marginTop:'30px',
+                            fontSize:'24px',
+                            marginBottom:'40px'
+                            }}
+                            >
+                            💣 ᴅɪᴡᴀʟɪ ᴅʜᴀᴍᴀᴋᴀ: ɢᴇᴛ ᴍᴏʀᴇ, ꜱᴘᴇɴᴅ ʟᴇꜱꜱ – ᴀʟʟ ɪᴛᴇᴍꜱ Upto <b>₹199</b>💣
+                        </section>
+                    </div>
+                </div>
+                <div className="col-lg-4 col-sm-4 col-md-4 col-xs-4 ">
                 <Lottie
-                animationData={MP4Animations}
-                className='animate__animated animate__bounce'
-                style={{
-                width: "200px",
-                margin: "0 auto",
-                }}
-                />
-            </div>
-            <div className="col-lg-4 col-sm-4 col-md-4">
-                <div className="icon-container">
-                    <section
-                        className='animate__animated animate__backInRight'
-                        style=
-                        {{textAlign:'center',
-                        marginTop:'30px',
-                        fontSize:'24px',
-                        marginBottom:'40px'
-                        }}
-                        >
-                        💣 ᴅɪᴡᴀʟɪ ᴅʜᴀᴍᴀᴋᴀ: ɢᴇᴛ ᴍᴏʀᴇ, ꜱᴘᴇɴᴅ ʟᴇꜱꜱ – ᴀʟʟ ɪᴛᴇᴍꜱ Upto <b>₹199</b>💣
-                    </section>
+                    animationData={MP4Animations}
+                    className='animate__animated animate__bounce bulb'
+                    style={{
+                    width: "200px",
+                    margin: "0 auto",
+                    }}
+                    />
                 </div>
             </div>
-            <div className="col-lg-4 col-sm-4 col-md-4">
-            <Lottie
-                animationData={MP4Animations}
-                className='animate__animated animate__bounce'
-                style={{
-                width: "200px",
-                margin: "0 auto",
-                }}
-                />
+            <div className="row justify-content-center mb-2">
+                    {renderImage()}
             </div>
         </div>
-        <div className="row justify-content-center mb-2">
-                {renderImage()}
-                </div>
-    </div>
+    </MainComponet>
   )
 }
+const MainComponet = styled.div`
+@media screen and (max-width: 575px) {
+    .col-xs-6 {
+      flex: 0 0 50%;
+      max-width: 50%;
+    }
+  }
+    @media screen and (max-width: 576px) {
+        .bulb{
+            display: none;
+        }
+    }
+`
 
 export default MainPage
---
+++ src/elements/images.tsx
@@ -0,0 +1,176 @@
+import {
+  IMG1,
+  IMG2,
+  IMG3,
+  IMG4,
+  IMG5,
+  IMG6,
+  IMG7,
+  IMG10,
+  IMG11,
+  IMG13,
+  IMG8,
+  IMG16,
+  IMG17,
+  IMG20,
+  IMG22,
+  IMG23,
+  IMG24,
+  IMG25,
+  IMG26,
+  IMG27,
+  IMG28,
+  IMG29,
+  IMG30,
+  IMG35,
+  IMG38,
+  IMG14,
+  IMG19,
+  IMG15,
+  IMG31,
+  IMG32,
+  IMG33,
+  IMG34,
+  IMG36,
+  IMG37,
+} from "../images";
+
+const images = [
+  {
+    image: IMG26,
+    title: "Press",
+  },
+  {
+    image: IMG25,
+    title: "Press",
+  },
+  {
+    image: IMG27,
+    title: "Not Press",
+  },
+  {
+    image: IMG22,
+    title: "Press",
+  },
+  {
+    image: IMG17,
+    title: "Press",
+  },
+  {
+    image: IMG16,
+    title: "Press",
+  },
+  {
+    image: IMG29,
+    title: "Press",
+  },
+  {
+    image: IMG1,
+    title: "Not Press",
+  },
+  {
+    image: IMG30,
+    title: "Not Press",
+  },
+  {
+    image: IMG14,
+    title: "Press",
+  },
+  {
+    image: IMG19,
+    title: "Not Press",
+  },
+  {
+    image: IMG15,
+    title: "Press",
+  },
+  {
+    image: IMG31,
+    title: "Press",
+  },
+  {
+    image: IMG3,
+    title: "Press",
+  },
+  {
+    image: IMG32,
+    title: "Not Press",
+  },
+  {
+    image: IMG11,
+    title: "Press",
+  },
+  {
+    image: IMG8,
+    title: "Press",
+  },
+  {
+    image: IMG13,
+    title: "Press",
+  },
+  {
+    image: IMG33,
+    title: "Press",
+  },
+  {
+    image: IMG4,
+    title: "Press",
+  },
+  {
+    image: IMG34,
+    title: "Press",
+  },
+  {
+    image: IMG2,
+    title: "Press",
+  },
+  {
+    image: IMG7,
+    title: "NotPress",
+  },
+  {
+    image: IMG10,
+    title: "Not Press",
+  },
+  {
+    image: IMG35,
+    title: "Press",
+  },
+  {
+    image: IMG5,
+    title: "Press",
+  },
+  {
+    image: IMG36,
+    title: "Press",
+  },
+  {
+    image: IMG23,
+    title: "Press",
+  },
+  {
+    image: IMG20,
+    title: "Press",
+  },
+  {
+    image: IMG24,
+    title: "Press",
+  },
+  {
+    image: IMG37,
+    title: "Press",
+  },
+  {
+    image: IMG6,
+    title: "Not Press",
+  },
+  {
+    image: IMG38,
+    title: "Press",
+  },
+  {
+    image: IMG28,
+    title: "Not Press",
+  },
+];
+export default images;
